2f588f0a66eff682f450b580d5a32eb63c3d2b31
Alon Cohen <alon@cohencodes.com>
2019-07-12 23:36:32 -0700

    added user component and updated nav

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,24 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO use express middleware to handle cookies (JWT)
-// TODO use express middleware to populate current user
+server.express.use(cookieParser());
+
+// decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userId onto the req for future requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
@@ -15,6 +28,6 @@ server.start(
     }
   },
   deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
